05a7cc27209ffdfb7ba58f17fccb21a8800b2b9b
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-11-19 09:23:24 +0530

    Complete db class

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ class Database:
     def __init__(self, db):
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS parts (id, INTERGER PRIMARY KEY, part text, customer text, retailer text, price text)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS parts (id INTERGER PRIMARY KEY, part text, customer text, retailer text, price text)")
         self.conn.commit()
     
 
